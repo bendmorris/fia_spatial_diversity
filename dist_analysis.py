496ed966d73969001e607393e0ef8b22f20341d2
bendmorris <ben@bendmorris.com>
2013-04-04 21:42:17 -0400

    Simplifying.

--- dist_analysis.py
+++ dist_analysis.py
@@ -28,84 +28,85 @@ def distance(p1, p2):
     c = 2 * math.atan2(a**0.5, (1-a)**0.5)
     return 6371 * c
 
-def main():
-    tree = bp.read('fia.newick', 'newick')
-    #tree.get_path = metrics.get_path_with_cache
-    tree = metrics.CachingTree(tree)
+tree = bp.read('fia.newick', 'newick')
+#tree.get_path = metrics.get_path_with_cache
+tree = metrics.CachingTree(tree)
 
 
-    # read in route/species abundance information from FIA data file
-    routes = {}
-    all_species = set()
-    with open('fia.csv') as data_file:
-        reader = csv.reader(data_file)
-        # skip header row
-        next(reader)
-        for lat, lon, genus, species, count in reader:
-            lat, lon = float(lat), float(lon)
-            count = int(count)
-            if species == 'unknown': continue
+# read in route/species abundance information from FIA data file
+routes = {}
+all_species = set()
+with open('fia.csv') as data_file:
+    reader = csv.reader(data_file)
+    # skip header row
+    next(reader)
+    for lat, lon, genus, species, count in reader:
+        lat, lon = float(lat), float(lon)
+        count = int(count)
+        if species == 'unknown': continue
 
-            species_name = '%s %s' % (genus, species)
-            all_species.add(species_name)
-            route = (lat,lon)
+        species_name = '%s %s' % (genus, species)
+        all_species.add(species_name)
+        route = (lat,lon)
 
-            loc = (lat, lon)
-            if not loc in routes:
-                routes[loc] = {}
-            routes[loc][species_name] = count
+        loc = (lat, lon)
+        if not loc in routes:
+            routes[loc] = {}
+        routes[loc][species_name] = count
 
-    # get the range of lat/lon values
-    lats, lons = [route[0] for route in routes], [route[1] for route in routes]
-    lat_range = (min(lats), max(lats))
-    lon_range = (min(lons), max(lons))
+# get the range of lat/lon values
+lats, lons = [route[0] for route in routes], [route[1] for route in routes]
+lat_range = (min(lats), max(lats))
+lon_range = (min(lons), max(lons))
 
 
-    def analyze(arg):
-        bin_size, results = arg
-        
-        while len(results) < COMPARISONS:
+def analyze(arg):
+    bin_size, results = arg
+    
+    while len(results) < COMPARISONS:
+        r1, r2 = random.sample(routes, 2)
+        while not bin_size <= distance(r1, r2) <= bin_size + BIN_SIZE:
             r1, r2 = random.sample(routes, 2)
-            while not bin_size <= distance(r1, r2) <= bin_size + BIN_SIZE:
-                r1, r2 = random.sample(routes, 2)
-                
-            lat_range = min((r1[0], r2[0])), max((r1[0], r2[0]))
-            lon_range = min((r1[1], r2[1])), max((r1[1], r2[1]))
             
-            species_pool = []
-            for lat, lon in routes:
-                if (lat_range[0] <= lat <= lat_range[1] and
-                    lon_range[0] <= lon <= lon_range[1]):
-                    for sp, count in routes[lat,lon].iteritems():
-                        species_pool += [sp] * count
-
-            try:
-                # compute beta NTI
-                nti = metrics.beta_nti(routes[r1], routes[r2], tree, 
-                                       verbose=False, reps=1000)
-                if nti == np.nan: continue
-
-                if abs(nti) >= 2:
-                    # abs(beta NTI) >= 2 indicates selection
-                    result = 'selection'
-                else:
-                    # < 2: raup-crick to determine drift or dispersal
-                    rc = metrics.raup_crick(routes[r1], routes[r2], species_pool)
-                    if rc <= -0.95:
-                        result = 'homogenizing dispersal'
-                    elif rc >= 0.95:
-                        result = 'dispersal limitation'
-                    else:
-                        result = 'drift'
-            except IndexError:
-                # this means a species wasn't found in our tree
-                continue
-            
-            print bin_size, result
-            results.append(result)
+        lat_range = min((r1[0], r2[0])), max((r1[0], r2[0]))
+        lon_range = min((r1[1], r2[1])), max((r1[1], r2[1]))
         
-        return (bin_size, results)
+        species_pool = []
+        for lat, lon in routes:
+            if (lat_range[0] <= lat <= lat_range[1] and
+                lon_range[0] <= lon <= lon_range[1]):
+                for sp, count in routes[lat,lon].iteritems():
+                    species_pool += [sp] * count
 
+        try:
+            # compute beta NTI
+            nti = metrics.beta_nti(routes[r1], routes[r2], tree, 
+                                   verbose=False, reps=1000)
+            if nti == np.nan: continue
+
+            if abs(nti) >= 2:
+                # abs(beta NTI) >= 2 indicates selection
+                result = 'selection'
+            else:
+                # < 2: raup-crick to determine drift or dispersal
+                rc = metrics.raup_crick(routes[r1], routes[r2], species_pool)
+                if rc <= -0.95:
+                    result = 'homogenizing dispersal'
+                elif rc >= 0.95:
+                    result = 'dispersal limitation'
+                else:
+                    result = 'drift'
+        except IndexError:
+            # this means a species wasn't found in our tree
+            continue
+        
+        print bin_size, result
+        results.append(result)
+    
+    return (bin_size, results)
+
+
+if __name__ == '__main__':
     results = multiprocessing.Pool().map(analyze, result_bins.iteritems())
     results = dict(results)
     results = {a: {type: 100.*len([x for x in b if x == type])/len(b) 
@@ -114,7 +115,3 @@ def main():
 
     with open('dist_results.pkl', 'w') as results_file:
         pkl.dump(results, results_file, -1)
-
-
-if __name__ == '__main__':
-    main()
--- grid_analysis.py
+++ grid_analysis.py
@@ -15,105 +15,106 @@ COMPARISONS = 1000
 MIN_SITES = 50
 
 
-def main():
-    tree = bp.read('fia.newick', 'newick')
-    #tree.get_path = metrics.get_path_with_cache
-    tree = metrics.CachingTree(tree)
+tree = bp.read('fia.newick', 'newick')
+#tree.get_path = metrics.get_path_with_cache
+tree = metrics.CachingTree(tree)
 
-    # read in route/species abundance information from FIA data file
-    grids = {}
-    all_species = set()
-    with open('fia.csv') as data_file:
-        reader = csv.reader(data_file)
-        # skip header row
-        next(reader)
-        for lat, lon, genus, species, count in reader:
-            lat, lon = float(lat), float(lon)
-            count = int(count)
-            if species == 'unknown': continue
+# read in route/species abundance information from FIA data file
+grids = {}
+all_species = set()
+with open('fia.csv') as data_file:
+    reader = csv.reader(data_file)
+    # skip header row
+    next(reader)
+    for lat, lon, genus, species, count in reader:
+        lat, lon = float(lat), float(lon)
+        count = int(count)
+        if species == 'unknown': continue
 
-            species_name = '%s %s' % (genus, species)
-            all_species.add(species_name)
-            route = (lat,lon)
+        species_name = '%s %s' % (genus, species)
+        all_species.add(species_name)
+        route = (lat,lon)
 
-            grid = (int(lat), int(lon))
-            if not grid in grids:
-                grids[grid] = {}
-            routes = grids[grid]
+        grid = (int(lat), int(lon))
+        if not grid in grids:
+            grids[grid] = {}
+        routes = grids[grid]
 
-            if not route in routes:
-                routes[route] = {}
-            routes[route][species_name] = count
+        if not route in routes:
+            routes[route] = {}
+        routes[route][species_name] = count
 
-    grids = {x:y for x, y in grids.iteritems() if len(y) > MIN_SITES}
-    print len(grids), 'total grids'
-    # get the range of lat/lon values
-    lats, lons = [route[0] for route in routes], [route[1] for route in routes]
-    lat_range = (min(lats), max(lats))
-    lon_range = (min(lons), max(lons))
+grids = {x:y for x, y in grids.iteritems() if len(y) > MIN_SITES}
+print len(grids), 'total grids'
+# get the range of lat/lon values
+lats, lons = [route[0] for route in routes], [route[1] for route in routes]
+lat_range = (min(lats), max(lats))
+lon_range = (min(lons), max(lons))
 
 
-    results = {}
-    def analyze(arg):
-        grid, routes = arg
-        species_pool = []
-        for route in routes.values():
-            for sp, count in route.iteritems(): 
-                species_pool += [sp] * count
+results = {}
+def analyze(arg):
+    grid, routes = arg
+    species_pool = []
+    for route in routes.values():
+        for sp, count in route.iteritems(): 
+            species_pool += [sp] * count
 
-        n = len(routes)
-        
-        # compare all combinations of routes if n choose 2 < COMPARISONS,
-        # otherwise compare random combinations until you reach COMBINATIONS
-        # total comparisons
-        comparisons = n*(n-1) / 2
-        if comparisons < COMPARISONS:
-            to_compare = ((r1, r2) for r1 in routes for r2 in routes if not r1 == r2)
-        else:
-            def random_comparison():
-                while True:
-                    yield tuple(sorted(random.sample(routes.keys(), 2)))
-            to_compare = random_comparison()
-        
-        # compare pairs of communities
-        comms = []
-        while len(comms) < min(COMPARISONS, comparisons):
-            try: r1, r2 = next(to_compare)
-            except StopIteration: break
-        
-            if len(routes[r1]) < 2 or len(routes[r2]) < 2: continue
-        
-            try:
-                # compute beta NTI
-                nti = metrics.beta_nti(routes[r1], routes[r2], tree, 
-                                       verbose=False, reps=1000)
-                if nti == np.nan: continue
+    n = len(routes)
+    
+    # compare all combinations of routes if n choose 2 < COMPARISONS,
+    # otherwise compare random combinations until you reach COMBINATIONS
+    # total comparisons
+    comparisons = n*(n-1) / 2
+    if comparisons < COMPARISONS:
+        to_compare = ((r1, r2) for r1 in routes for r2 in routes if not r1 == r2)
+    else:
+        def random_comparison():
+            while True:
+                yield tuple(sorted(random.sample(routes.keys(), 2)))
+        to_compare = random_comparison()
+    
+    # compare pairs of communities
+    comms = []
+    while len(comms) < min(COMPARISONS, comparisons):
+        try: r1, r2 = next(to_compare)
+        except StopIteration: break
+    
+        if len(routes[r1]) < 2 or len(routes[r2]) < 2: continue
+    
+        try:
+            # compute beta NTI
+            nti = metrics.beta_nti(routes[r1], routes[r2], tree, 
+                                   verbose=False, reps=1000)
+            if nti == np.nan: continue
 
-                if abs(nti) >= 2:
-                    # abs(beta NTI) >= 2 indicates selection
-                    result = 'selection'
+            if abs(nti) >= 2:
+                # abs(beta NTI) >= 2 indicates selection
+                result = 'selection'
+            else:
+                # < 2: raup-crick to determine drift or dispersal
+                rc = metrics.raup_crick(routes[r1], routes[r2], species_pool)
+                if rc <= -0.95:
+                    result = 'homogenizing dispersal'
+                elif rc >= 0.95:
+                    result = 'dispersal limitation'
                 else:
-                    # < 2: raup-crick to determine drift or dispersal
-                    rc = metrics.raup_crick(routes[r1], routes[r2], species_pool)
-                    if rc <= -0.95:
-                        result = 'homogenizing dispersal'
-                    elif rc >= 0.95:
-                        result = 'dispersal limitation'
-                    else:
-                        result = 'drift'
-                comms.append(result)
-            except IndexError:
-                # this means a species wasn't found in our tree
-                pass
-        
-        results = {}
-        for result in sorted(set(comms)):
-            percent = 100*len([c for c in comms if c == result]) / float(len(comms))
-            print '%s: %s%%' % (result, percent)
-            results[result] = percent
-        
-        return grid, results
+                    result = 'drift'
+            comms.append(result)
+        except IndexError:
+            # this means a species wasn't found in our tree
+            pass
+    
+    results = {}
+    for result in sorted(set(comms)):
+        percent = 100*len([c for c in comms if c == result]) / float(len(comms))
+        print '%s: %s%%' % (result, percent)
+        results[result] = percent
+    
+    return grid, results
 
+
+if __name__ == '__main__':
     results = dict([
                     result for result in 
                     multiprocessing.Pool().map(analyze, grids.iteritems())
@@ -122,7 +123,3 @@ def main():
 
     with open('grid_results.pkl', 'w') as results_file:
         pkl.dump(results, results_file, -1)
-    
-    
-if __name__ == '__main__':
-    main()
